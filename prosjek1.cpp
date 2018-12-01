62469fdb796885dd9e09556f313f2432f548cd3f
bush-man92 <42248918+bush-man92@users.noreply.github.com>
2018-12-01 18:14:53 +0100

    gefufna

---
+++ prosjek1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int a = 1;
+	vector<int> vektor;
+	int prosjek = 0;
+	for (int i = 0; i < a;){
+		cin >> a;
+		vektor.push_back(a);
+	}
+	for (int i = 0; i < vektor.size(); i++) {
+		prosjek += vektor[i];
+	}
+	cout << setprecision(2) << fixed << float(prosjek) / (vektor.size() - 1);
+	
+	return 0;
+	
+}
