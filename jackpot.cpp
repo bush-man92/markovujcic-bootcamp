84816fbac90e833414e6ab0d7c8c2788c07b467e
bush-man92 <marko.vujcic92@gmail.com>
2018-12-06 19:37:25 +0100

    jackpot

--- jackpot.cpp
+++ jackpot.cpp
@@ -1,21 +1,57 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-	vector<int> dobitno;
-	int N;
+	string dobitak = "";
+	vector<string> listici;
 	
 	for (int i = 0; i < 6; i++) {
+		string N = "";
 		cin >> N;
-		dobitno.push_back(N);
+		dobitak += N[N.size() - 1];
 	}
 	
-	for (int i = 0; i < 6; i++) {
-		dobitno[i] = dobitno[i] % 10;
-		cout << dobitno[i];
+	for (int i = 0; i < 3; i++) {
+		string listic;
+		cin >> listic;
+		listici.push_back(listic);
 	}
-
+	
+	cout << dobitak << endl;
+	
+	for (int j = 0; j < 3; j++){
+		int counter;
+		for (int i = 5; i >= 0; i--) {
+			if (listici[j][i] == dobitak[i]) {
+				counter += 1;
+			}
+			else if (listici[j][i] != dobitak[i]) {
+				break;
+			}
+		}
+		if (counter == 6) {
+			cout << "I. vrsta" << endl;
+		}
+		else if (counter == 5) {
+			cout << "II. vrsta" << endl;
+		}
+		else if (counter == 4) {
+			cout << "III. vrsta" << endl;
+		}
+		else if (counter == 3) {
+			cout << "IV. vrsta" << endl;
+		}
+		else if (counter == 2) {
+			cout << "V. vrsta" << endl;
+		}
+		else {
+			cout << "Nedobitan" << endl;
+		}
+		cout << counter << endl;
+		counter = 0;
+	}
 }
